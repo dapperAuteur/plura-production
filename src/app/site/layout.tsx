718e4eeec5ce2b13e8347ddd748a91ae6ab3b188
dapperAuteur <a@awews.com>
2024-02-10 13:17:14 -0700

    just to deploy

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -2,6 +2,7 @@ import Navigation from '@/components/site/navigation'
 import { ClerkProvider } from '@clerk/nextjs'
 import { dark } from '@clerk/themes'
 import React from 'react'
+// si
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
